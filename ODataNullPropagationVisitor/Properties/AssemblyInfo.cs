b8512c3dd47e33048d4c40dbf292311ce41cde83
David Fowler <davidfowl@gmail.com>
2011-08-07 21:05:02 -0700

    Build nuget package and symbols package on build

--- ODataNullPropagationVisitor/Properties/AssemblyInfo.cs
+++ ODataNullPropagationVisitor/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ODataNullPropagationVisitor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fix for odata's aggressive null propagation logic")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("David Fowler")]
 [assembly: AssemblyProduct("ODataNullPropagationVisitor")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © David Fowler 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
