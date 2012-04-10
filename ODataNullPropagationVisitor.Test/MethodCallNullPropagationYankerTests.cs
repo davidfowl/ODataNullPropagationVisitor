3cf83aa7c693ed16cf10411e10e1d6915141d2f0
ray2k <ray.oneill@gmail.com>
2012-04-10 02:36:28 -0400

    added methodcall null propagation yanking and test project

---
+++ ODataNullPropagationVisitor.Test/MethodCallNullPropagationYankerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using OData.Linq;
+using System;
+using QueryInterceptor;
+
+namespace ODataNullPropagationVisitor.Test
+{
+    public class MethodCallNullPropagationYankerTests
+    {
+        public class Product
+        {
+            public string Name { get; set; }
+        }
+
+        [Fact]
+        public void Should_Remove_Null_Propgation_In_MethodCall_Expressions()
+        {
+            List<Product> productList = new List<Product>()
+            {
+                new Product() { Name = "Product 1" },
+                new Product() { Name = null }
+            };
+
+            var productsQueryable = productList.AsQueryable();
+
+            var withPropagation = (from p in productsQueryable
+                                   where (p.Name == null ? "product 1" : p.Name.ToLower()) == "product 1"
+                                   select p).Expression;
+
+            var withoutPropagation = (from p in productsQueryable
+                                      where p.Name.ToLower() == "product 1"
+                                      select p).Expression;
+
+            var propagationRemoved = new MethodCallNullPropagationYanker().Visit(withPropagation);
+
+            string withPropagationString = SimpleExpressionPrinter.Stringify(withPropagation);
+            string withoutPropagationString = SimpleExpressionPrinter.Stringify(withoutPropagation);
+            string propagationRemovedString = SimpleExpressionPrinter.Stringify(propagationRemoved);
+
+            Assert.NotEqual(withPropagationString, propagationRemovedString);
+            Assert.Equal(withoutPropagationString, propagationRemovedString);
+        }
+    }
+}
---
+++ ODataNullPropagationVisitor.Test/SimpleExpressionPrinter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace ODataNullPropagationVisitor.Test
+{
+    internal class SimpleExpressionPrinter : ExpressionVisitor {
+        private StringBuilder _sb = new StringBuilder();
+
+        public override Expression Visit(Expression node) {
+            if (node == null)
+                return node;
+
+            _sb.AppendFormat(" {0}", node.GetType().Name);
+            return base.Visit(node);
+        }
+
+        public static string Stringify(Expression node) {
+            var printer = new SimpleExpressionPrinter();
+            printer.Visit(node);
+            return printer._sb.ToString().Trim();
+        }
+    }
+}
